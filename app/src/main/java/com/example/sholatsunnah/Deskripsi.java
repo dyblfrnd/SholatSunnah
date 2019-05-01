2d76bb9e26bbb972122b13422e2482b8b8f0c630
SAFARAZ <safaraz@SSDs-MacBook-Pro-584.local>
2019-05-01 22:18:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sholatsunnah/Deskripsi.java
@@ -0,0 +1,25 @@
+package com.example.sholatsunnah;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Deskripsi extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deskripsi);
+
+        webView = findViewById(R.id.webView);
+
+        Intent deskripsi = getIntent();
+
+        String link = deskripsi.getStringExtra("tahjjud");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+
+    }
+}
