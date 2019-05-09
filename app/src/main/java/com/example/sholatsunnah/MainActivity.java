65e39e4ba682c9a85e5b9dd85ee943c33f697369
dyblfrnd <safaraz.afma.affandi@gmail.com>
2019-05-09 08:41:13 +0700

    initial commit

--- app/src/main/java/com/example/sholatsunnah/MainActivity.java
+++ app/src/main/java/com/example/sholatsunnah/MainActivity.java
@@ -1,14 +1,11 @@
 
-package com.gharin.solatsunnah;
+package com.example.sholatsunnah;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import com.example.sholatsunnah.Deskripsi;
-import com.example.sholatsunnah.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 //        todo : deklarasi
